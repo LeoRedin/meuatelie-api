55868fe1d9cd38eeb79e3cdcd78554b57f066028
Leonardo Redin <redin@hey.com>
2023-12-13 13:59:12 -0300

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './configs/database.config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...databaseConfig,
     }),
+
+    // Modules
+    UserModule,
   ],
   controllers: [],
   providers: [],
